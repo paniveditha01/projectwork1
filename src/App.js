a0722fce509839d4dd9f836c8cea034ea2db956f
P A Niveditha <panivedithaniveditha@gmail.com>
2023-11-20 15:02:06 +0530

    first time

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import Navigation from './Components/Navigation'
+import {BrowserRouter as Router, Routes,Route} from 'react-router-dom'
+import Home from './Components/Home'
+import Contact from './Components/Contact'
+import  TodoList from'./Components/TodoList'
+import NavBar from './Components/NavBar'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <div>
+      <NavBar/>
     </div>
-  );
+    <Routes>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/contact'element={<Contact/>}/>
+      <Route path='/todolist' element={<TodoList/>}/>
+
+    </Routes>
+    </Router>
+    </div>
+  )
 }
 
-export default App;
+export default App
